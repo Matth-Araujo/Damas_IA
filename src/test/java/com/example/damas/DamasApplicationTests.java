c0bdffd20ff720d002a9e4fac00572a78c229da6
Matheus <matheusaraujo220@gmail.com>
2025-09-21 23:06:39 -0300

    Base

---
+++ src/test/java/com/example/damas/DamasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.damas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
